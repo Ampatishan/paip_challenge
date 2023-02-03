cf4473fab1f89ccb4fec6b8d7d0a3973276e8958
ampatishan <aampatishan@gmail.com>
2023-02-03 04:11:41 -0700

    changed the code so the validation dataset can be without masks

--- utils/dataset.py
+++ utils/dataset.py
@@ -6,7 +6,7 @@ from torch.utils.data import Dataset
 # TODO Incorporate the values from the excel file into the dataset file
 
 class paip_dataset(Dataset):
-    def __init__(self, is_colon, image_path, mask_path, transforms=None, \
+    def __init__(self, is_colon, image_path, mask_path=None, transforms=None, \
                  target_transfrom=None):
         self.img_path = image_path
         self.mask_path = mask_path
@@ -14,10 +14,12 @@ class paip_dataset(Dataset):
         self.target_transform = target_transfrom
         self.is_colon = is_colon
         self.img_names = os.listdir(self.img_path)
-        self.tumor_masks_names = os.listdir(os.path.join( \
-            self.mask_path, 'tumor'))
-        self.non_tumor_masks_names = os.listdir(os.path.join( \
-            self.mask_path, 'non_tumor'))
+
+        if mask_path:
+            self.tumor_masks_names = os.listdir(os.path.join( \
+                self.mask_path, 'tumor'))
+            self.non_tumor_masks_names = os.listdir(os.path.join( \
+                self.mask_path, 'non_tumor'))
 
         if is_colon:
             self.select_part('c')
@@ -25,31 +27,39 @@ class paip_dataset(Dataset):
             self.select_part('p')
 
     def __len__(self):
-        assert len(self.img_names) == len(self.non_tumor_masks_names) \
-               == len(self.tumor_masks_names), \
-            "Number of files in images and masks doesn't match"
+        if self.mask_path:
+            assert len(self.img_names) == len(self.non_tumor_masks_names) \
+                   == len(self.tumor_masks_names), \
+                "Number of files in images and masks doesn't match"
         return len(self.img_names)
 
     def __getitem__(self, idx):
+        idx = 0
         img_name = self.img_names[idx]
-        tumor_mask_name = self.tumor_masks_names[idx]
-        non_tumor_mask_name = self.non_tumor_masks_names[idx]
 
-        assert img_name[:7] == tumor_mask_name[:7] \
-               == non_tumor_mask_name[:7], \
-            "Non-corresponding files selected for masks and images"
+        if self.mask_path:
+            tumor_mask_name = self.tumor_masks_names[idx]
+            non_tumor_mask_name = self.non_tumor_masks_names[idx]
+
+            assert img_name[:7] == tumor_mask_name[:7] \
+                   == non_tumor_mask_name[:7], \
+                "Non-corresponding files selected for masks and images"
 
         img = cv2.imread(os.path.join(self.img_path, img_name))
-        tumor_mask = cv2.imread(os.path.join(self.mask_path, 'tumor', tumor_mask_name))
-        non_tumor_mask = cv2.imread(os.path.join(self.mask_path, 'non_tumor', non_tumor_mask_name))
-
         if self.transform:
             img = self.transform(img)
-        if self.target_transform:
-            tumor_mask = self.target_transform(tumor_mask)
-            non_tumor_mask = self.target_transform(non_tumor_mask)
 
-        return img, tumor_mask, non_tumor_mask
+        if self.mask_path:
+            tumor_mask = cv2.imread(os.path.join(self.mask_path, 'tumor', tumor_mask_name))
+            non_tumor_mask = cv2.imread(os.path.join(self.mask_path, 'non_tumor', non_tumor_mask_name))
+
+            if self.target_transform:
+                tumor_mask = self.target_transform(tumor_mask)
+                non_tumor_mask = self.target_transform(non_tumor_mask)
+
+            return img, tumor_mask, non_tumor_mask
+        else:
+            return img
 
     def select_part(self, part):
         self.img_names = [x for x in self.img_names if x[3] == part]
